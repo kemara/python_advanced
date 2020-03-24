494dec78faa95767cb23c56a7fcab9e31dd2bd16
Aleksandar Makragic <amakragic@apple.com>
2020-03-24 16:03:04 +0100

    Removing unecessary lines

--- The Python Data Model/example_1_1.py
+++ The Python Data Model/example_1_1.py
@@ -2,7 +2,6 @@ import collections
 
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
-
 class FrenchDeck:
     ranks = [str(n) for n in range(2, 12)] + list('JQKA')
     suits = 'spades diamonds clubs hearts'.split()
